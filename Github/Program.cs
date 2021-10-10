6df08d5e28fff8b3fe00c02cb976c4bc44d04253
shakedx <kolesnikov_danil123@mail.ru>
2021-10-10 15:35:32 +0200

    Добавьте файлы проекта.

---
+++ Github/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c:");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите d:");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите e:");
+            double e = double.Parse(Console.ReadLine());
+            Console.WriteLine("a={0} b={1}",a,b);
+
+
+
+
+        }
+    }
+}
